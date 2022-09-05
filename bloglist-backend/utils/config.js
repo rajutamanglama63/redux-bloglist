37e3b87770bac4bf426b8318eac8822f963c55f9
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-09-05 16:25:14 +0545

    feat: backend init

---
+++ bloglist-backend/utils/config.js
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+module.exports = {
+  PORT,
+};
---
+++ src/reducers/blogReducer.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import blogServices from "../services/blogs";
+
+const blogSlice = createSlice({
+  name: "blogs",
+  initialState: [],
+  reducers: {
+    setBlogs(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { setBlogs } = blogSlice.actions;
+
+export const initializeBlogs = () => {
+  return async (dispatch) => {
+    const blogs = await blogServices.getAll();
+    dispatch(setBlogs(blogs));
+  };
+};
+
+export default blogSlice.reducer;
