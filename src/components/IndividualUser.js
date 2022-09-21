9dd4c15730b8a033f753af828ed9207103953189
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-09-21 13:10:52 +0545

    feat: individual user component added

---
+++ src/components/IndividualUser.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const IndividualUser = () => {
+  return (
+    <div>
+      <h2>Dasy</h2>
+      <h4>added blogs</h4>
+
+      <ul>
+        <li>ghaintang</li>
+        <li>ghaintang</li>
+        <li>ghaintang</li>
+        <li>ghaintang</li>
+      </ul>
+    </div>
+  );
+};
+
+export default IndividualUser;
