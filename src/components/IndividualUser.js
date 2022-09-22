2941caec423f43db9ef228634ea9af803d1b78ef
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-09-22 20:26:09 +0545

    feat: individual rendering bug fixed

--- src/components/IndividualUser.js
+++ src/components/IndividualUser.js
@@ -3,26 +3,27 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../reducers/userReducer";
 
 const IndividualUser = () => {
-  const individualUser = useSelector((state) => {
-    console.log(state);
-  });
-  // console.log(individualUser);
+  const individualUser = useSelector((state) => state.individualUser);
+  console.log(individualUser);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getAllUsers());
-  }, [dispatch]);
   return (
-    <div>
-      <h2>{individualUser.name}</h2>
-      <h4>added blogs</h4>
+    <>
+      {individualUser ? (
+        <>
+          <div>
+            <h2>{individualUser.name}</h2>
+            <h4>added blogs</h4>
 
-      <ul>
-        {individualUser.blogs.map((blog) => (
-          <li key={blog.id}>{blog.title}</li>
-        ))}
-      </ul>
-    </div>
+            <ul>
+              {individualUser.blogs.map((blog) => (
+                <li key={blog.id}>{blog.title}</li>
+              ))}
+            </ul>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 };
 
